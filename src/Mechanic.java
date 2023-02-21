33c225dcfddb58a719cc19510b507ec655a1398c
ПК <ekrapostin@gmail.com>
2023-02-21 19:37:31 +0300

    Создал поля, конструктор, геттеры и сеттеры и методы в классе механник

--- src/Mechanic.java
+++ src/Mechanic.java
@@ -1,2 +1,33 @@
 public class Mechanic {
+    private String name;
+    private String company;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public Mechanic(String name, String company) {
+        this.name = name;
+        this.company = company;
+    }
+
+    public void maintenance() {
+        System.out.println("Механник проводит техобслуживание");
+    }
+
+    public void fixTransport() {
+        System.out.println("Механиик чинит транспорт");
+    }
 }
